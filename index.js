8556c2e391c3b90bfdcf3811c2922b1306d0542e
webdevelopermohiuddin <mohiuddinmazumder94@gmail.com>
2021-06-22 01:44:13 +0600

    ok

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const User = require('./model/user');
 const cors = require('cors');
 const fs = require('fs-extra');
 const fileUpload = require('express-fileupload');
-const MongoClient = require('mongodb').MongoClient;
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+// const MongoClient = require('mongodb').MongoClient;
 const mongoose = require('mongoose');
 require('dotenv').config();
 
@@ -13,6 +15,8 @@ require('dotenv').config();
 
 // const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.l47bs.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 // const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const JWT_SECRET = 'sl@%lkfj%$#34552sfofnsf34@@##$@#%$%23lfsjdflsfj slfjsldfj';
 
 mongoose.connect('mongodb://localhost:27017/login-app-db',{
   useNewUrlParser: true,
@@ -33,9 +37,51 @@ const port = 5000
 //   const jobsCollection = client.db(`${process.env.DB_NAME}`).collection("Jobs");
 
 
+ //user login api                   
+ app.post('/api/login', async (req, res) => {
+   const {email, password} = req.body;
+
+   const user = await User.findOne({email}).lean()
+
+   if(!user){
+     return res.json({status: "error", error: 'Invalid email/password'})
+   }
+
+   if(await bcrypt.compare(password, user.password)){
+     const token = jwt.sign({
+       id: user._id, 
+       email: user.email
+     }, 
+     JWT_SECRET
+   )
+        return res.json({status: "ok", data: ''})
+   }
+
+
+     res.json({status: 'error', error: "Invalid email/password"});
+ })   
+
   // user registration api
   app.post('/api/register', async(req, res)=> {
     console.log(req.body);
+
+    const {name, email, password, role, companyName} = req.body;
+
+    console.log(await bcrypt.hash(password, 10))
+
+    try{
+       const response = await User.create({
+         name,
+         password,
+         email,
+         role,
+         companyName 
+       })
+       console.log("User created successfully", response)
+    }catch(error){
+      console.log(error)
+      return res.json({ status: 'error' })
+    }
     
     res.json({ status: 'ok' })
   })
--- model/user.js
+++ model/user.js
@@ -2,8 +2,11 @@ const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema(
     {
-    username: {type: String, required: true, unique: true},
-    password: {type: String, required: true}
+    name: {type: String, required: true},
+    password: {type: String, required: true},
+    email: {type: String, required: true},
+    role: {type: String, required: true},
+    companyName: {type: String},
     },
     {collection: 'users'}
 )
